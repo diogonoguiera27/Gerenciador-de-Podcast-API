b754940816c08159fe7b3f678a3424c6c9b49b33
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-11-05 14:54:13 -0300

    Criando a service

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,21 +1,8 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { serviceListEpisodes } from "../services/list-episodes-service";
 
-export const getListEpisodes = (req: IncomingMessage, res: ServerResponse) => {
+export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
+  const content = await serviceListEpisodes()
   res.writeHead(200, { "content-type": "application/json" });
-  res.end(
-    JSON.stringify([
-      {
-        podcastName: "flow",
-        episode: "CBUM - Flow #319",
-        videoId: "QSuQmUfS30",
-        category: ["saude", "bodybuilder"],
-      },
-      {
-        podcastName: "flow",
-        episode: "RUBENS BARRICHELLO - Flow #339",
-        videoId: "QSuQmUfS30",
-        category: ["esporte", "corrida"],
-      },
-    ])
-  );
+  res.end(JSON.stringify(content));
 };
---
+++ src/services/list-episodes-service.ts
@@ -0,0 +1,17 @@
+export const serviceListEpisodes = async () => {
+  const data = [
+    {
+      podcastName: "flow",
+      episode: "CBUM - Flow #319",
+      videoId: "QSuQmUfS30",
+      category: ["saude", "bodybuilder"],
+    },
+    {
+      podcastName: "flow",
+      episode: "RUBENS BARRICHELLO - Flow #339",
+      videoId: "QSuQmUfS30",
+      category: ["esporte", "corrida"],
+    },
+  ];
+  return data 
+};
