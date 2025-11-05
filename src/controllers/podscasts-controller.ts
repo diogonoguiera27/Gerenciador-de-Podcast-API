d137a179101d45b0acc38ed43b2e35d794173b25
diogonoguiera27 <diogoalvesnogueira3@gmail.com>
2025-11-05 14:33:24 -0300

    Chamada da api com simuladores de clients postman , insomina , thunder cliente dentro do vs code para fazer teste da api

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,8 +1,21 @@
-import { IncomingMessage , ServerResponse} from "http"
+import { IncomingMessage, ServerResponse } from "http";
 
-export const getListEpisodes = (req : IncomingMessage, res: ServerResponse) => {
-   res.writeHead(200, {"content-type": "application/json"}) ;
-   res.end({
-    name: "Diogo",
-   });
-}
+export const getListEpisodes = (req: IncomingMessage, res: ServerResponse) => {
+  res.writeHead(200, { "content-type": "application/json" });
+  res.end(
+    JSON.stringify([
+      {
+        podcastName: "flow",
+        episode: "CBUM - Flow #319",
+        videoId: "QSuQmUfS30",
+        category: ["saude", "bodybuilder"],
+      },
+      {
+        podcastName: "flow",
+        episode: "RUBENS BARRICHELLO - Flow #339",
+        videoId: "QSuQmUfS30",
+        category: ["esporte", "corrida"],
+      },
+    ])
+  );
+};
